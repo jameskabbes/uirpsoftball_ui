5736ccbe2d565500afecc1fecb4c3c2f77106921
James Kabbes <james.kabbes@gmail.com>
2025-06-01 16:16:50 -0500

    updating component

--- src/components/Game/createEvent.ts
+++ src/components/Game/createEvent.ts
@@ -6,21 +6,18 @@ function createEvent(
   game: components['schemas']['GameExport'],
   location: components['schemas']['LocationExport'] | null,
   homeTeam: components['schemas']['TeamExport'] | null,
-  awayTeam: components['schemas']['TeamExport'] | null,
-  officiatingTeam: components['schemas']['TeamExport'] | null
+  awayTeam: components['schemas']['TeamExport'] | null
 ): string {
   let date = DateTime.fromISO(game.datetime).setZone('UTC');
   return `BEGIN:VEVENT
 DTSTART:${date.toFormat('yyyyLLdd')}T${date.toFormat('HHmmss')}Z
 DURATION:PT1H
 SUMMARY:UIRP Softball Game
-LOCATION:${location?.name ?? 'Location TBD'}: ${
-    location?.link ?? ''
-  }DESCRIPTION:${homeTeam?.name ?? 'Home Team TBD'} vs. ${
+LOCATION:${location?.name ?? 'Location TBD'}: ${location?.link ?? ''}
+DESCRIPTION:${homeTeam?.name ?? 'Home Team TBD'} vs. ${
     awayTeam?.name ?? 'Away Team TBD'
-  }\\nUmpires: ${
-    officiatingTeam?.name ?? 'Officiating Team TBD'
-  }\\nGame Details: https://${config.frontendUrl}/game/${game.id}
+  }\\nGame Details: ${config.frontendUrl}/games/${game.id}
+URL: ${config.frontendUrl}/games/${game.id}
 END:VEVENT`;
 }
 
